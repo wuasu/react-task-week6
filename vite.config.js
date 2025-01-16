4bcbc8da6b60e486d0d8eb352a39b18cc2de9e23
Wanyu <q37375@gmail.com>
2025-01-17 01:11:33 +0800

    改成react-task-week3

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   //課前:將 Vite 專案打包並部署 8:00
-  base: process.env.NODE_ENV === 'production' ? '/react-task-week2/' : '/',
+  base: process.env.NODE_ENV === "production" ? "/react-task-week3/" : "/",
   plugins: [react()],
 });
